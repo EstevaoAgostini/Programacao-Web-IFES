5e4480cdd4cca0ef50dff3eb59d030a5c0e02595
Estevão Nunes Agostini <estevaoagostini@gmail.com>
2026-08-24 18:06:48 -0300

    quase terminando

--- Programacao Web III/Exercícios/Exercício 5 - Array/script.js
+++ Programacao Web III/Exercícios/Exercício 5 - Array/script.js
@@ -1,24 +1,48 @@
 //Atividade 1
 function escolhas() {
-    const equipes = ["Ferrari", "Mercedes", "McLaren", "RedBull", "Audi"]
+    const equipes = ["Ferrari", "Mercedes", "McLaren", "RedBull", "Racing Bulls", "Alpine", "Hass", "Audi", "Williams", "Aston Martin", "Cadillac"]
     let resultado = document.getElementById("escolha")
 
     resultado.value = ""
 
     for (let i = 0; i < equipes.length; i++) {
-        resultado.value += `Minha escolha ${i + 1} é ${equipes[i]} \n`
+        resultado.value += `Minha escolha ${i + 1} é ${equipes[i]}. \n`
     }
 }
 
 //Atividade 2
 function quadrado() {
-    const numeros = [1, 2, 3, 4, 5]
-    const quadrados = numeros.map(function(numero) {return numero ** 2})
-    let resultado = document.getElementById("quadrado") 
+    const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    const quadrados = numeros.map(function (numero) { return numero ** 2 })
+    let resultado = document.getElementById("quadrado")
 
     resultado.value = ""
 
     for (let i = 0; i < quadrados.length; i++) {
-        resultado.value += `O quadrado de ${numeros[i]} é ${quadrados[i]} \n`
+        resultado.value += `O quadrado de ${numeros[i]} é ${quadrados[i]}. \n`
+    }
+}
+
+//Atividade 3
+function romanos() {
+    const listaNumeros = [1,2,3,4,5,6,7,8,9,10,11];
+    const valores = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanos = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    let resultado = document.getElementById("romano");
+    resultado.value = "";
+
+    for (let j = 0; j < listaNumeros.length; j++) {
+        let numeroAtual = listaNumeros[j];
+        let textoRomano = "";
+
+        for (let i = 0; i < valores.length; i++) {
+            while (numeroAtual >= valores[i]) {
+                textoRomano += romanos[i];
+                numeroAtual -= valores[i];
+            }
+        }
+
+        resultado.value += `${listaNumeros[j]} em romano é: ${textoRomano}\n`;
     }
 }
